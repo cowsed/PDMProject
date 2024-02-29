82c9bf79b709a67ebfa945b9e445330fba929981
cowsed <richie.sommers@outlook.com>
2024-02-29 12:00:57 -0500

    postgresql through ssh tunnel using psycopg2

---
+++ credentials.py
@@ -0,0 +1,6 @@
+ssh_username='cs username you use to get into the cs machines' #not including @rit.edu
+ssh_password='cs username you use to get into the cs machines'
+
+# if these are different for you, fill them in here
+cs_username=ssh_username
+cs_password=ssh_password
