2032453884b7dd0143bd114e16727fa3ace200b6
Alexis Sanders <ars9249@rit.edu>
2024-03-19 11:43:47 -0400

    added get_collection

--- backend/collection.py
+++ backend/collection.py
@@ -1,5 +1,6 @@
 from game import GameID
 from typing import List
+from database import cs_database
 
 class CollectionID:
     id: int
@@ -14,5 +15,16 @@ class Collection:
 
 
 def get_collection(col: CollectionID) -> CollectionID:
-    raise NotImplementedError
-    return 0
+    try:
+        with cs_database() as db:
+            query = '''select C.title, 
+                       (select count(*) as num_of_games from CollectionContains CC where CC.collectionID=%s), 
+                       (select sum(PG.endtime - PG.starttime) as play_time from PlaysGame PG 
+                            where PG.username=%s and PG.gameID in 
+                            (select CC.gameID from CollectionContains CC where CC.collectionID=%s))''', col, "", col
+            cursor = db.cursor()
+            cursor.execute(query)
+            result = cursor.fetchone()
+            return result
+    except:
+        return
