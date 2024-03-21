64a14734dfd7dc8c797c08779ca9d1315aa12fec
WhoDaPenguin <61098297+WhoDaPenguin@users.noreply.github.com>
2024-03-21 15:04:28 -0400

    Finished the game search

--- backend/game.py
+++ backend/game.py
@@ -1,5 +1,7 @@
 import datetime
 from enum import Enum
+import random
+import string
 from database import cs_database
 
 class GameID:
@@ -31,25 +33,59 @@ class Game:
 	id: GameID
 	rating: ESRBRating
 
-def search_games(title: str):
-	# SEARCH BY: name, platform, release date, developers, price, and genre
-	# RETURNS: name, platforms, the developers, the publisher, the playtime, and the ratings
+def search_games(title="", platform="", release_date_range=(datetime.date(1800, 1, 1), datetime.date.today()), developers="", price_range=(0.0, float('inf')), genre=""):
+	"""
+	Returns every game that matches the provided search criteria.
+
+	Parameters
+    ----------
+    title : str, default: ""
+        The search string to compare to each video game title.
+	platform : str, default: ""
+		The search string to compare to each platform title.
+	release_date_range : tuple, default: ( datetime.date(1800, 1, 1), datetime.date.today() )
+        The range each returned games' release date must fall into.
+	developers : str, default: ""
+		The search string to compare to each developer each game has.
+	price_range : tuple, default: (0.0, float('inf'))
+		 The range each returned games' price must fall into (varies by platform release).
+	genre : str, default: ""
+		The search string to compare to each genre each game falls into.
+
+	Returns
+	-------
+	list
+		A list of each matched game's name, platforms, developers, publisher, playtime, and ratings.
+		Each game release on a different platform is considered a different game and listed separately.
+	"""
+
 	try:
-		print("search moment")
 		with cs_database() as db:
 			# TODO get reviews, and playtime
 			query = '\
 				SELECT G.title, \
-					   (SELECT name FROM "Platform" WHERE platformid=GOP.platformid), \
-					   array(SELECT developer FROM "Development" WHERE gameid=G.gameid), \
+					   (SELECT name FROM "Platform" WHERE platformid=GOP.platformid) AS platform, \
+					   ARRAY(SELECT developer FROM "Development" WHERE gameid=G.gameid) AS developers, \
 					   G.publisher, \
 					   G.esrb_rating \
 				FROM "Game" G \
 				NATURAL JOIN "GameOnPlatform" GOP \
-				WHERE UPPER(title) LIKE UPPER(%s) \
+				WHERE UPPER(G.title) LIKE UPPER(%s) \
+				  AND UPPER((SELECT name FROM "Platform" WHERE platformid=GOP.platformid)) LIKE UPPER(%s) \
+				  AND GOP.release_date >= %s AND GOP.release_date <= %s \
+				  AND UPPER(ARRAY_TO_STRING(ARRAY(SELECT developer FROM "Development" WHERE gameid=G.gameid), \',\')) LIKE UPPER(%s) \
+				  AND GOP.price >= %s AND GOP.price <= %s \
+				  AND UPPER(ARRAY_TO_STRING(ARRAY(SELECT genre_name FROM "Genre" Ge WHERE G.gameid=Ge.gameid), \',\')) LIKE UPPER(%s) \
 			'
 			cursor = db.cursor()
-			cursor.execute(query, ('%' + title + '%',))
+			cursor.execute(query, ('%' + title + '%', 
+						  		   '%' + platform + '%', 
+								   release_date_range[0],
+								   release_date_range[1],
+								   '%' + developers + '%',
+								   price_range[0],
+								   price_range[1],
+								   '%' + genre + '%'))
 			result = cursor.fetchall()
 			return result
 	except Exception as e:
@@ -60,7 +96,8 @@ def search_games(title: str):
 def get_all_games():
 	try:
 		with cs_database() as db:
-			query = 'select G.title, G.esrb_rating, G.publisher from "Game" G'
+			# query = 'select G.title, G.esrb_rating, G.publisher from "Game" G'
+			query = 'select G.title from "Game" G'
 			cursor = db.cursor()
 			cursor.execute(query)
 			result = cursor.fetchall()
@@ -77,6 +114,37 @@ def find_game_by_title(title: str):
 			cursor = db.cursor()
 			cursor.execute(query, (title,))
 			result = cursor.fetchone()
+			return result
+	except Exception as e:
+		print(e)
+		# No such user found (or database down)
+		return None
+
+def get_all_development():
+	try:
+		with cs_database() as db:
+			query = 'SELECT (SELECT title FROM "Game" G WHERE D.gameid=G.gameid), \
+				            developer \
+					 FROM "Development" D'
+			cursor = db.cursor()
+			cursor.execute(query)
+			result = cursor.fetchall()
+			return result
+	except Exception as e:
+		print(e)
+		# No such user found (or database down)
+		return None
+	
+def get_all_game_on_platform():
+	try:
+		with cs_database() as db:
+			query = 'SELECT (SELECT name FROM "Platform" P WHERE P.platformid=GOP.gameid), \
+			                (SELECT title FROM "Game" G WHERE G.gameid=GOP.gameid) \
+					 FROM "GameOnPlatform" GOP'
+			query = 'SELECT * FROM "GameOnPlatform"'
+			cursor = db.cursor()
+			cursor.execute(query)
+			result = cursor.fetchall()
 			return result
 	except Exception as e:
 		print(e)
@@ -129,8 +197,8 @@ def add_game_to_platform(game_id="", game_title="", platform_id="", platform_tit
 			elif game_title != "" and platform_title != "":
 				query = '\
 					INSERT INTO "GameOnPlatform" (gameid, platformid, price, release_date) \
-					VALUES ((SELECT gameid FROM "Game" WHERE title=%s), \
-				            (SELECT platformid FROM "Platform" WHERE name=%s), %s, %s) \
+					VALUES ((SELECT gameid FROM "Game" WHERE title=%s LIMIT 1), \
+				            (SELECT platformid FROM "Platform" WHERE name=%s LIMIT 1), %s, %s) \
 				'
 				cursor = db.cursor()
 				cursor.execute(query, (game_title, platform_title, price, release_date))
@@ -142,7 +210,7 @@ def add_game_to_platform(game_id="", game_title="", platform_id="", platform_tit
 
 def add_developer_to_game(game_id="", game_title="", developer_name=""):
 	"""
-	Adds an entry to "GameOnPlatform" for the game with the specified id/title and the specified platform.
+	Adds an entry to "Development" for the game with the specified id/title and the specified developer.
 
 	Parameters
     ----------
@@ -150,14 +218,8 @@ def add_developer_to_game(game_id="", game_title="", developer_name=""):
         If specified, the method adds the entry based on ID.
 	game_title : str, default: ""
 		If specified and game_id not specified, the method adds the entry based on the game title.
-	platform_id : str, default: ""
-        If specified, the method adds the entry based on ID.
-	platform_title : str, default: ""
-		If specified and platform_id not specified, the method adds the entry based on the platform title.
-	price : float, default: 0.00
-		The price of the game on this platform.
-	release_date : datetime.date, default: datetime.date(2000, 1, 1)
-		The date the game was released on this platform.
+	developer_name : str, default: ""
+		The name of the developer to be assigned to the game.
 	"""
 
 	try:
@@ -173,10 +235,45 @@ def add_developer_to_game(game_id="", game_title="", developer_name=""):
 			else:
 				query = '\
 					INSERT INTO "Development" (gameid, developer) \
-					VALUES ((SELECT gameid FROM "Game" WHERE title=%s), %s) \
+					VALUES ((SELECT gameid FROM "Game" WHERE title=%s LIMIT 1), %s) \
 				'
 				cursor = db.cursor()
 				cursor.execute(query, (game_title, developer_name))
+				db.commit()
+	except Exception as e:
+		print(e)
+
+def add_genre_to_game(game_id="", game_title="", genre=""):
+	"""
+	Adds an entry to "Genre" for the game with the specified id/title and the specified genre.
+
+	Parameters
+    ----------
+    game_id : str, default: ""
+        If specified, the method adds the entry based on ID.
+	game_title : str, default: ""
+		If specified and game_id not specified, the method adds the entry based on the game title.
+	genre : str, default: ""
+		The name of the genre to be assigned to the game.
+	"""
+
+	try:
+		with cs_database() as db:
+			if game_id != "":
+				query = '\
+					INSERT INTO "Genre" (gameid, genre_name) \
+					VALUES (%s, %s) \
+				'
+				cursor = db.cursor()
+				cursor.execute(query, (game_id, genre))
+				db.commit()
+			else:
+				query = '\
+					INSERT INTO "Genre" (gameid, genre_name) \
+					VALUES ((SELECT gameid FROM "Game" WHERE title=%s LIMIT 1), %s) \
+				'
+				cursor = db.cursor()
+				cursor.execute(query, (game_title, genre))
 				db.commit()
 	except Exception as e:
 		print(e)
--- ui/main_page.py
+++ ui/main_page.py
@@ -4,5 +4,22 @@ from backend import player, game, platform
 
 def begin():
 	print("starting")
-	print(game.search_games("p"))
-	#print(game.get_all_games())
+	games = game.search_games(title="vermintide", genre="Co-op")
+	for vgame in games:
+		print(vgame)
+
+	# developers = game.get_all_development()
+	# for developer in developers:
+	# 	print(developer)
+	
+	# platforms = platform.get_all_platforms()
+	# for pform in platforms:
+	# 	print(pform)
+
+	# games = game.get_all_games()
+	# for vgame in games:
+	# 	print(vgame)
+
+	# platforms = game.get_all_game_on_platform()
+	# for game_on_platform in platforms:
+	# 	print(game_on_platform)
