3b9fabf169bbafb5b919e10b4c71755f1d018026
Tiana Heid <tmh1703@rit.edu>
2024-03-28 20:19:59 -0400

    view, update, and delete ratings

--- ui/library.py
+++ ui/library.py
@@ -1,5 +1,6 @@
 from backend.player import Player
 from backend.game import get_owned_games, get_game, GID
+from backend.owns_game import get_ratings, add_rating, delete_rating
 from typing import Dict
 import urwid
 
@@ -26,7 +27,7 @@ class LibraryPage:
         self.widget = urwid.Filler(urwid.Pile(parts))
 
     def backpressed(self, b: urwid.Button):
-        self.switch_menu("back", {})
+        self.switch_menu("main", {})
 
     def pressed(self, b: urwid.Button, dat: GID):
         self.switch_menu("library.onegame", {"gameid": dat})
@@ -41,12 +42,20 @@ class ViewOnePage:
         self.game = get_game(self.gameid)
         self.back_btn = urwid.Button("Back", self.pressed)
         self.record_time = urwid.Button("Record Playtime", self.pressed)
+        self.change_rating = urwid.Button("Change Rating", self.pressed)
+        self.delete_rating = urwid.Button("Delete Rating", self.pressed)
+        self.rating = get_ratings(self.gameid, self.player.username)
 
         parts = [
             self.back_btn,
-            urwid.Text("Game: "+self.game.name),
+            urwid.Text("Game: " + self.game.name),
             urwid.Divider(),
             self.record_time,
+            # show ratings
+            urwid.Text("\nRating:\n" + str(self.rating.star_rating) + " stars"),
+            urwid.Text(self.rating.review_text),
+            self.change_rating,
+            self.delete_rating
         ]
 
         self.widget = urwid.Filler(urwid.Pile(parts))
@@ -57,3 +66,43 @@ class ViewOnePage:
         elif b == self.record_time:
             self.switch_menu("library.onegame.record_time",
                              {"game": self.game, "gid": self.game.id})
+        elif b == self.change_rating:
+            self.switch_menu("library.onegame.change_rating",
+                             {"game": self.game, "gid": self.game.id})
+        elif b == self.delete_rating:
+            delete_rating(self.gameid, self.player.username)
+
+
+class ChangeRating:
+    def __init__(self, switch_menu, player: Player, args: Dict):
+        self.switch_menu = switch_menu
+        self.player = player
+
+        self.header = urwid.Text("Change Rating")
+        self.star = urwid.Edit("Number of Stars: ")
+        self.rating = urwid.Edit("Review: ")
+        self.gameid = args["gid"]
+        self.game = get_game(self.gameid)
+        self.back_btn = urwid.Button("Back", self.pressed)
+
+        parts = [
+            self.header,
+            urwid.Divider(),
+
+            self.star,
+            self.rating,
+            urwid.Divider(),
+
+            urwid.Button("Change", self.pressed, "change"),
+            urwid.Button("Back", self.pressed, self.game.id)
+        ]
+
+        self.widget = urwid.Filler(urwid.Pile(parts))
+
+    def pressed(self, b: urwid.Button, dat: GID):
+        if dat == "change":
+            star = self.star.get_edit_text()
+            rating = self.rating.get_edit_text()
+            add_rating(self.gameid, self.player.username, int(star), rating)
+        else:
+            self.switch_menu("library.onegame", {"gameid": dat})
